360bef76d83d4c224dbf3df5d6741f643499a0d9
Pablo Arias <>
2018-02-06 18:24:14 +0100

    read_me with dependencies

---
+++ super_vp_path.py
@@ -0,0 +1,14 @@
+# --------------------------------------------------------------------#
+# --------------------------------------------------------------------#
+# ---------- Made by pablo Arias @ircam on 11/2015
+# ---------- Analyse audio and return soudn features
+# ---------- to us this don't forget to include these lines before your script:
+# ----------
+# ---------- import sys
+# ---------- sys.path.append('/Users/arias/Documents/Developement/Python/')
+# ---------- from parse_sdif import 'the functions you need'
+# ----------
+# --------------------------------------------------------------------#
+# --------------------------------------------------------------------#
+AudioSculpt_version = "3.4.5"
+super_vp_path = "/Applications/AudioSculpt\ "+AudioSculpt_version+"/AudioSculpt\ "+AudioSculpt_version+".app/Contents/MacOS/supervp"
