14b3ce0285ccc271059402a4a46d4865822561db
daniel bedoya <daniel.bedoya@ircam.fr>
2018-09-27 13:59:57 +0200

    - Minor changes to licence characters : "Sorbonne Universite" - Ask if alpha is int or float - Changed ts_formants.praat to its path in STIM (still needs to be fixed to refer to Praat app in any OS; currently it only refers to MacOS)

--- super_vp_path.py
+++ super_vp_path.py
@@ -1,7 +1,7 @@
 # --------------------------------------------------------------------#
 # --------------------------------------------------------------------#
 # ---------- Made by pablo Arias @ircam on 11/2015
-# ---------- Copyright (c) 2018 CREAM Lab // CNRS / IRCAM / Sorbonne Université
+# ---------- Copyright (c) 2018 CREAM Lab // CNRS / IRCAM / Sorbonne Universite
 # ----------
 # ---------- Analyse audio and return soudn features
 # ---------- to us this don't forget to include these lines before your script:
